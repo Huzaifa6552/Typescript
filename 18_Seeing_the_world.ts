af0688d957c1f78f13a92d962566b467997bb3b6
Huzaifa6552 <137796307+Huzaifa6552@users.noreply.github.com>
2024-03-14 11:08:17 +0500

    Add files via upload

---
+++ 18_Seeing_the_world.ts
@@ -0,0 +1,27 @@
+let places : string [] = ['Itle','Germany','agar','Eifle Tower','Times Square'];
+
+console.log('Orignal' + places);
+
+console.log('Copy ' +[ ...places].sort);
+
+
+console.log('Orignal' + places);
+
+console.log('copy '+ [...places].sort().reverse());
+
+console.log('Orignal ' + places);
+console.log('Orignal ' + places.reverse());
+
+console.log('Orignal ' + places.reverse());
+console.log('Orignal' + places);
+
+
+
+console.log('Orignal ' + places.sort().reverse());
+
+
+
+
+
+
+
