5158f47072d6614bffcfdf5a5a4114e424ac352d
Huzaifa6552 <137796307+Huzaifa6552@users.noreply.github.com>
2024-03-14 10:33:00 +0500

    Add files via upload

---
+++ persnal massge.js.js
@@ -0,0 +1,9 @@
+"use strict";
+let personName = '';
+personName = prompt('whit is your name?') || '';
+if (personName !== null && personName !== '') {
+    alert('Hello faizan would you like to learn pythn today');
+}
+else {
+    alert('you have to fill your name');
+}
