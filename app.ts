4305d98d14e7cd856e48605726a20b87d950aad3
Huzaifa6552 <137796307+Huzaifa6552@users.noreply.github.com>
2024-03-14 10:27:44 +0500

    Delete app.ts

--- app.ts
+++
@@ -1,9 +0,0 @@
-let personName: string ='';
-
-personName = prompt('whit is your name?') || '';
-if(personName !== null && personName !== '' ){
-    alert('Hello faizan would you like to learn pythn today');
-}
-else{
-    alert('you have to fill your name');
-}
