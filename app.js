348c57bcb53aec087dde1903b35e14f77e081f37
Huzaifa6552 <137796307+Huzaifa6552@users.noreply.github.com>
2024-03-14 10:27:11 +0500

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-let personName = '';
-personName = prompt('whit is your name?') || '';
-if (personName !== null && personName !== '') {
-    alert('Hello faizan would you like to learn pythn today');
-}
-else {
-    alert('you have to fill your name');
-}
