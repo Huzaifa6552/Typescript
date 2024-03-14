5c65f1041c948eab3e0e79ee8a954fd47fa44811
Huzaifa6552 <137796307+Huzaifa6552@users.noreply.github.com>
2024-03-14 11:03:17 +0500

    Add files via upload

---
+++ 14_ Guest_list.ts
@@ -0,0 +1,8 @@
+
+
+let guest_list :string [] = ['imran khan','nawaz sharif','Asif',];
+
+for(let i=0; i<guest_list.length; i++){
+    console.log('Dera Mr.' + guest_list[i] + ', \n\n it is our pleasure to invite you in our party. \n\n Thank you!');
+}
+
