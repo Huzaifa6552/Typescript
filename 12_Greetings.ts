a9fb423fdf1ccac7f0c927fa8ba1659d88388341
Huzaifa6552 <137796307+Huzaifa6552@users.noreply.github.com>
2024-03-14 11:01:22 +0500

    Add files via upload

---
+++ 12_Greetings.ts
@@ -0,0 +1,7 @@
+
+
+let Names :string[] = ['usman','arslan','rehan','faizan'];
+const message :string = 'Walcome to typescript class:'
+for(let i=0; i<Names.length; i++){
+
+    console.log(message+Names[i]);}
